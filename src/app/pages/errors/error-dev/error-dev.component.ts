60cd05329c55b844fe78a7ae4df1a75540c8a4d1
VILA Loris (SNCF VOYAGEURS / DIRECTION DE L'INGENIERIE DU MATERIEL / CIC - OPMV AGC) <loris.vila@sncf.fr>
2025-01-05 11:53:44 +0100

    feat(router): Added the "still in dev" page + updated router

---
+++ src/app/pages/errors/error-dev/error-dev.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-dev',
+  templateUrl: './error-dev.component.html',
+  styleUrl: './error-dev.component.css'
+})
+export class ErrorDevComponent {
+
+}
