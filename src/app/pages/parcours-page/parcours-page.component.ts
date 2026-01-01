697bcbb7cb04f9448153ab7b26f78963765b2ca8
Loris VILA (SNCF VOYAGEURS / DIRECTION DE L'INGENIERIE DU MATERIEL / CIC - OPTER AGC) <loris.vila@outlook.fr>
2026-01-01 18:00:01 +0100

    feat: Implementation of a new customized progress timeline element

--- src/app/pages/parcours-page/parcours-page.component.ts
+++ src/app/pages/parcours-page/parcours-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomStepConfig, CustomStepClickEvent } from '../../shared/custom-horizontal-stepper/custom-horizontal-stepper.interface';
 
 @Component({
   selector: 'app-parcours-page',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './parcours-page.component.css'
 })
 export class ParcoursPageComponent {
+
+  public currentStep: number = 0;
+  public steps: CustomStepConfig[] = [
+    { text: 'Opérateur Essieux TGV', icon: 'build' },
+    { text: 'Opérateur Essais-Dépannages', icon: 'troubleshoot' },
+    { text: 'Diplôme Bac Pro MEI', icon: 'school' },
+    { text: 'Technicien Essais-Dépannages', icon: 'troubleshoot' },
+    { text: 'Diplôme BTS MS', icon: 'school' },
+    { text: 'Ingénieur Systémier OpTER AGC', icon: 'engineering' },
+    { text: 'Diplôme d\'ingénieur ICAM SNI', icon: 'check' },
+  ];
+
+  onStepClick(event: CustomStepClickEvent): void {
+    this.currentStep = event.index;
+  }
+
+  nextStep() {
+    if (this.currentStep < this.steps.length - 1) {
+      this.currentStep++;
+    }
+  }
+
+  previousStep() {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+    }
+  }
 
 }
